b7802beb0d8644268f3c8d67c984525c2e50e84c
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-02-28 14:06:41 +0000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <search-form/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchForm from './components/SearchForm.vue';
+import {eventBus} from './main.js'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'search-form': SearchForm
+  },
+  data() {
+    return {
+      articles: [],
+      searchString: ""
+    }
   }
 }
 </script>
---
+++ src/components/SearchForm.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <div class="search-form">
+    <label for="search-input">Search for articles:</label>
+    <input type="text" name="search-input" v-model="searchString"/>
+    <button type="button" name="searchbutton" @click="handleSearch">Search</button>
+  </div>
+
+</template>
+
+<script>
+import {eventBus} from '../main.js';
+
+export default {
+  name: 'search-form',
+  data(){
+    return {
+      searchString: ""
+    }
+  },
+  handleSearch: function () {
+    eventBus.$emit('search-string', this.searchString)
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
